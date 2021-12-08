9bfbfa2ada3e4660187f4411bfc2e3ffb07fd2ca
djho57 <daniel.j.ho57@gmail.com>
2021-12-07 19:37:57 -0600

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,24 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'camry',
+        mileage: 415000,
+        title: 'salvage',
+    },
+    {
+        vin: '1111111111111',
+        make: 'dodge',
+        model: 'charger',
+        mileage: 31000,
+    },
+]
